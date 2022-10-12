77a50ae6f46a54af05bf79b1f76f493a7b534341
Aleksandar Karastoyanov <karastoqnov.alexandar@gmail.com>
2022-10-12 16:20:34 +0300

    Test One SNOW Conn

--- front-end/src/testOne.js
+++ front-end/src/testOne.js
@@ -2,11 +2,11 @@ function testOne() {
   const sn = require("servicenow-rest-api"); // ServiceNow API request
 
   const ServiceNow = new sn("dev109438", "admin", "LrmsjVJB@8^3"); // Call SNOW client with credentials using the request
-  ServiceNow.Authenticate(); // Authentication with provide credentials
+  ServiceNow.Authenticate(); // Authentication with provided credentials
 
-  ServiceNow.getSampleData("sys_user", (res) => { // Get data from any table/records
-  });
+  ServiceNow.getSampleData("sys_user", (res) => {}); // Get data from any table/records
 
+  // Specify the fields for the query
   const fields = [
     "first_name",
     "last_name",
@@ -14,12 +14,17 @@ function testOne() {
     "active",
   ];
 
+  // Create filters for specific fields and values
   const filters = [
-    "first_name=survey",
+    "first_name=John",
+    "last_name=Doe",
   ];
 
+  // Execute query to the CMDB with the fields and filters created above.
   ServiceNow.getTableData(fields, filters, "sys_user", function (res) {
-    console.log(res);
+    // console.log(res[0]);
+    let queryResult = res[0];
+    console.log(queryResult);
   });
 }
 
