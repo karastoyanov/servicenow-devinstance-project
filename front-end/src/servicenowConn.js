122444f3eb218494ae296a087e9ab8fa08d5ef52
Aleksandar Karastoyanov <karastoqnov.alexandar@gmail.com>
2022-10-12 23:44:29 +0300

    SN connection fixed, testOne query getting back data

--- front-end/src/servicenowConn.js
+++ front-end/src/servicenowConn.js
@@ -1,12 +1,13 @@
-function snowConnection() {
-  const sn = require("servicenow-rest-api");
+// function snowConnection() {
+//   const sn = require("servicenow-rest-api");
+//   const ServiceNow = new sn("dev109438", "admin", "LrmsjVJB@8^3");
+//   module.exports = { ServiceNow };
+//   //ServiceNow.Authenticate();
+//   const connection = ServiceNow.Authenticate();
+// }
+// snowConnection();
 
-  const ServiceNow = new sn("dev109438", "admin", "LrmsjVJB@8^3");
-
-  ServiceNow.Authenticate();
-  ServiceNow.getSampleData("u_project_verify", (res) => {
-    console.log(res);
-  });
-}
-
-snowConnection();
+const sn = require("servicenow-rest-api"); // Call the API
+const ServiceNow = new sn("dev109438", "admin", "LrmsjVJB@8^3"); // Create SN client with credentials
+module.exports = { ServiceNow }; // Exports the "ServiceNow" variable for later use 
+ServiceNow.Authenticate(); // Authenticate with SNOW instance
--- front-end/src/testOne.js
+++ front-end/src/testOne.js
@@ -1,31 +1,14 @@
-function testOne() {
-  const sn = require("servicenow-rest-api"); // ServiceNow API request
+function verifyTask() {
+  const { ServiceNow } = require("./servicenowConn.js"); // Import "ServiceNow" variable from servicenowConn.js
 
-  const ServiceNow = new sn("dev109438", "admin", "LrmsjVJB@8^3"); // Call SNOW client with credentials using the request
-  ServiceNow.Authenticate(); // Authentication with provided credentials
-
-  ServiceNow.getSampleData("sys_user", (res) => {}); // Get data from any table/records
-
-  // Specify the fields for the query
-  const fields = [
-    "first_name",
-    "last_name",
-    "email",
-    "active",
-  ];
-
-  // Create filters for specific fields and values
-  const filters = [
-    "first_name=John",
-    "last_name=Doe",
-  ];
-
-  // Execute query to the CMDB with the fields and filters created above.
-  ServiceNow.getTableData(fields, filters, "sys_user", function (res) {
-    // console.log(res[0]);
-    let queryResult = res[0];
-    console.log(queryResult);
+  // Get data from table
+  ServiceNow.getSampleData('u_project_verify', (res) => {
+    for (let i = 0; i < res.length; i++) {
+      const object = res[i];
+      for (const key in object) {
+        // TO DO if-conditions
+      }
+    }  
   });
 }
-
-testOne();
+verifyTask();
